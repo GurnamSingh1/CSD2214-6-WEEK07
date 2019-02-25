1a6ea952539053966e7ee6959ab2f85418f37e2b
Gurnamsingh <visualstudio@230-PC40>
2019-02-25 15:31:22 -0500

    3. 30 commit

--- C0741350_Gurnam/js/main.js
+++ C0741350_Gurnam/js/main.js
@@ -1,1 +1,22 @@
 ﻿// Your code here!
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven";
+    callType: "squawky",
+    quote: "Nevermore",
+    maxoffspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear";
+    callType: "roar",
+    quote: "",
+    maxoffspring: 3,
+    noisy: true,
+    deadly: true
+};
